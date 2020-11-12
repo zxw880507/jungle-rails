36cbf34ad99014ab6703e30d9db26cc4d8359ff7
Calvin Zheng <calvinzheng@Calvins-MBP.hitronhub.home>
2020-11-11 23:12:28 -0700

    "Adding about page"

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get 'about', action: :show, controller: 'about'
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
